3323371c9fd10e76c33104035e21d01f7b42e15e
mohamadhandy <108653540+mohamadhandy@users.noreply.github.com>
2022-11-17 09:10:07 +0000

    separate component category item and modify sample data to have images

---
+++ src/components/category-item/CategoryItem.jsx
@@ -0,0 +1,17 @@
+import "./CategoryItem.scss";
+import React from "react";
+
+const CategoryItem = ({ category: { title, imageUrl } }) => (
+  <div className="category-container">
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    ></div>
+    <div key={title} className="category-body-container">
+      <h2>{title}</h2>
+      <p>Shop Now</p>
+    </div>
+  </div>
+);
+
+export default CategoryItem;

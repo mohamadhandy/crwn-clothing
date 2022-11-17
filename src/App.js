762985665f9fa0239d72255c037dd71c3eb2d8e7
mohamadhandy <108653540+mohamadhandy@users.noreply.github.com>
2022-11-17 06:37:52 +0000

    initialize crown clothing projects

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  const categories = [
+    { categoryName: "Hats", shopName: "Shop Now" },
+    { categoryName: "Jackets", shopName: "Shop Now" },
+    { categoryName: "Sneakers", shopName: "Shop Now" },
+    { categoryName: "Women", shopName: "Shop Now" },
+    { categoryName: "Men", shopName: "Shop Now" },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img src="" alt="Category image" /> */}
+        {categories.map((category) => (
+          <div key={category.categoryName} className="category-body-container">
+            <h2>{category.categoryName}</h2>
+            <p>{category.shopName}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;

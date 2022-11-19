719791f560eb7a23339999b0fdd9b0292b9a0042
mohamadhandy <108653540+mohamadhandy@users.noreply.github.com>
2022-11-19 14:33:40 +0000

    finalize setup google popup signin

--- src/routes/signin/SignIn.jsx
+++ src/routes/signin/SignIn.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
@@ -7,8 +6,9 @@ import {
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
+    await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign in page</h1>
--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -21,23 +21,21 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+initializeApp(firebaseConfig);
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithgoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
 export const db = getFirestore();
 export const createUserDocumentFromAuth = async (userAuth) => {
   // check reference di firebasenya ada atau enggak
   const userDocRef = doc(db, "users", userAuth.uid);
-  console.log(userDocRef);
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot);
-  console.log(userSnapshot.exists());
 
   // kalau data tidak ada
   if (!userSnapshot.exists()) {

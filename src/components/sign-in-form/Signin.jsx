b018a27bbaef074eb13a8fc93d2f45ece9b04ad9
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-11-23 10:28:37 +0700

    create sign in functionalities & add formInput component

--- src/components/sign-in-form/Signin.jsx
+++ src/components/sign-in-form/Signin.jsx
@@ -1,24 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../button/Button";
 import HeaderSigninSignup from "../header-signin-signup/HeaderSigninSignup";
+import FormInput from "../form-input/FormInput";
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInUserWithEmailAndPassword,
 } from "../../utils/firebase/firebase";
+import { callSwal } from "../../utils/sweetalert/sweetalert";
+
+const defaultSigninFields = {
+  email: "",
+  password: "",
+};
 
 const Signin = () => {
-  const logGoogleUser = async () => {
+  const [signInFields, setSigninFields] = useState(defaultSigninFields);
+  const { email, password } = signInFields;
+  const signInWithGoogle = async () => {
     const { user } = await signInWithGooglePopup();
     await createUserDocumentFromAuth(user);
   };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSigninFields({ ...signInFields, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { user } = await signInUserWithEmailAndPassword(email, password);
+      if (user.uid) {
+        callSwal(
+          "Success Signin",
+          `Welcome ${email} again to crwn clothing`,
+          "success"
+        );
+      }
+    } catch (error) {
+      if (error.code) {
+        callSwal("Error occurs", `Error ${error.code}`, "error");
+      }
+    }
+  };
   return (
     <div>
-      <HeaderSigninSignup
-        title={"I already have an account"}
-        subTitle={"Sign in with your email and password"}
-      />
-      <Button buttonType={"google"} onClick={logGoogleUser}>
-        Sign in with google popup
+      <form onSubmit={handleSubmit}>
+        <HeaderSigninSignup
+          title={"I already have an account"}
+          subTitle={"Sign in with your email and password"}
+        />
+        <FormInput
+          label={"Email"}
+          id={"emailSignin"}
+          type="email"
+          onChange={handleChange}
+          name={"email"}
+          value={email}
+        />
+        <FormInput
+          label={"Password"}
+          id={"passwordSignin"}
+          type="password"
+          onChange={handleChange}
+          name={"password"}
+          value={password}
+        />
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <Button buttonType={"google"} onClick={signInWithGoogle}>
+        Sign in with Google
       </Button>
     </div>
   );
--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -7,6 +7,7 @@ import {
   signInWithRedirect,
   signInWithPopup,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
   GoogleAuthProvider,
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -66,4 +67,9 @@ export const createUserDocumentFromAuth = async (
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
   return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password);
 };

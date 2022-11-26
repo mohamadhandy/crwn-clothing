b8d542701bcee47c0c18841db2f8add8b60de4b2
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-11-27 00:51:18 +0700

    add increase qty

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -4,10 +4,13 @@ import { CartContext } from "../../contexts/CartContext";
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, price, quantity, imageUrl } = cartItem;
-  const { removeItemFromCart } = useContext(CartContext);
+  const { removeItemFromCart, addItemToCart } = useContext(CartContext);
   const handleRemove = () => {
     removeItemFromCart(cartItem);
   };
+  const handleIncreaseQty = () => {
+    addItemToCart(cartItem);
+  }
   return (
     <div className="checkout-item-container">
       <div className="image-container">
@@ -17,7 +20,7 @@ const CheckoutItem = ({ cartItem }) => {
       <span className="quantity">
         <div className="arrow">&#10094;</div>
         <span className="value">{quantity}</span>
-        <div className="arrow">&#10095;</div>
+        <div className="arrow" onClick={handleIncreaseQty}>&#10095;</div>
       </span>
       <span className="price"> {price}</span>
       <div className="remove-button" onClick={handleRemove}>

ef0fe3eaafae76ed0f7e435fdbb19172e6d19524
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-11-27 21:53:38 +0700

    improvement using useNavigate

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -3,10 +3,14 @@ import "./CartDropdown.scss";
 import Button from "../button/Button";
 import CartItem from "../cart-item/CartItem";
 import { CartContext } from "../../contexts/CartContext";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
+  const navigate = useNavigate();
   const { cartItems } = useContext(CartContext);
+  const goToCheckoutPage = () => {
+    navigate("/checkout");
+  };
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -14,9 +18,7 @@ const CartDropdown = () => {
           <CartItem cartItem={item} key={item.id} />
         ))}
       </div>
-      <Link to={"/checkout"}>
-        <Button>Checkout</Button>
-      </Link>
+      <Button onClick={goToCheckoutPage}>Checkout</Button>
     </div>
   );
 };

f74fec577a74b8ec5bad401db6cded16c9d3913d
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-11-29 10:46:09 +0700

    fix handleclick for category

--- src/components/category-preview/CategoryPreview.jsx
+++ src/components/category-preview/CategoryPreview.jsx
@@ -1,21 +1,30 @@
 import ProductCard from "../product-card/ProductCard";
 import "./CategoryPreview.scss";
+import { useNavigate } from "react-router-dom";
 
 import React from "react";
 
-const CategoryPreview = ({ title, products }) => (
-  <div className="category-preview-container">
-    <h2>
-      <span className="title">{title?.toUpperCase()}</span>
-    </h2>
-    <div className="preview">
-      {products
-        .filter((_, index) => index < 4)
-        .map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+const CategoryPreview = ({ title, products }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/shop/${title}`);
+  };
+  return (
+    <div className="category-preview-container">
+      <h2>
+        <span onClick={handleClick} className="title">
+          {title?.toUpperCase()}
+        </span>
+      </h2>
+      <div className="preview">
+        {products
+          .filter((_, index) => index < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CategoryPreview;

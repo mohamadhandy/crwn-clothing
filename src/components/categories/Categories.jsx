0c09f0302d9b550614016c25d685018080b5f233
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-11-30 16:25:54 +0700

    Add categories styled

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,13 +1,13 @@
-import "./categories.scss";
+import { CategoriesContainer } from "./Categories-styled";
 import React from "react";
 import CategoryItem from "../category-item/CategoryItem";
 
 const Categories = ({ categories }) => (
-  <div className="categories-container">
+  <CategoriesContainer className="categories-container">
     {categories.map((category, index) => (
       <CategoryItem category={category} key={index} />
     ))}
-  </div>
+  </CategoriesContainer>
 );
 
 export default Categories;

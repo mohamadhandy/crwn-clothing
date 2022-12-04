2db9bdd20d2dacaff0af8dc4d92182dc17f16f84
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-12-04 20:59:10 +0700

    create styled component for CheckoutItem

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -1,35 +1,42 @@
 import React, { useContext } from "react";
-import "./CheckoutItem.scss";
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  NameQuantityPrice,
+  RemoveButton,
+  QuantityValue,
+  QuantityClass,
+  QuantityArrow,
+} from "./CheckoutItem-styled";
 import { CartContext } from "../../contexts/CartContext";
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, price, quantity, imageUrl } = cartItem;
-  const { removeItemFromCart, addItemToCart, decreaseItemQty } = useContext(CartContext);
+  const { removeItemFromCart, addItemToCart, decreaseItemQty } =
+    useContext(CartContext);
   const handleRemove = () => {
     removeItemFromCart(cartItem);
   };
   const handleIncreaseQty = () => {
     addItemToCart(cartItem);
-  }
+  };
   const handleDecreaseQty = () => {
     decreaseItemQty(cartItem);
-  }
+  };
   return (
-    <div className="checkout-item-container">
-      <div className="image-container">
+    <CheckoutItemContainer>
+      <ImageContainer>
         <img src={imageUrl} alt={`${name}`} />
-      </div>
-      <span className="name"> {name} </span>
-      <span className="quantity">
-        <div className="arrow" onClick={handleDecreaseQty}>&#10094;</div>
-        <span className="value">{quantity}</span>
-        <div className="arrow" onClick={handleIncreaseQty}>&#10095;</div>
-      </span>
-      <span className="price"> {price}</span>
-      <div className="remove-button" onClick={handleRemove}>
-        &#10005;
-      </div>
-    </div>
+      </ImageContainer>
+      <NameQuantityPrice>{name}</NameQuantityPrice>
+      <QuantityClass>
+        <QuantityArrow onClick={handleDecreaseQty}>&#10094;</QuantityArrow>
+        <QuantityValue>{quantity}</QuantityValue>
+        <QuantityArrow onClick={handleIncreaseQty}>&#10095;</QuantityArrow>
+      </QuantityClass>
+      <NameQuantityPrice>${price}</NameQuantityPrice>
+      <RemoveButton onClick={handleRemove}>&#10005;</RemoveButton>
+    </CheckoutItemContainer>
   );
 };
 
